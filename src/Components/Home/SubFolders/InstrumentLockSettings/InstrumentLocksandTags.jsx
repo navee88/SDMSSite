c1181a79d9967f6de798141b5bf6d97aa38e33e8
kirubhakaran78 <kirubhakaran.rajendran@agaramtech.com>
2025-12-12 12:49:26 +0530

    kirubhakaran

---
+++ src/Components/Home/SubFolders/InstrumentLockSettings/InstrumentLocksandTags.jsx
@@ -0,0 +1,9 @@
+export default function InstrumentLocksAndTags() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold mb-4">Instrument Locks and Tags</h2>
+      {/* Instrument Locks and Tags content goes here */}
+      <p>This is where you can manage instrument locks and tags.</p>
+    </div>
+  );
+}
