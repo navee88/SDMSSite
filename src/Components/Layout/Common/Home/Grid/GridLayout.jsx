40cd45d764ea11f440469f819f8acb26d21a7a3d
naveen.k <naveen.k@agaramtech.com>
2025-12-18 17:49:34 +0530

    changed some thing

--- src/Components/Layout/Common/Home/Grid/GridLayout.jsx
+++ src/Components/Layout/Common/Home/Grid/GridLayout.jsx
@@ -5,7 +5,7 @@ import { TbSortDescendingLetters } from "react-icons/tb";
 import { CgPlayListRemove } from "react-icons/cg";
 import { useTranslation } from "react-i18next";
 
-
+//Icons
 const SearchIcon = () => (
   <svg className="w-3 h-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
